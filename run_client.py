afb1b5ee24c60516e804ea91e85c75e9626dc282
Liu  Li Chen <issac.liu@mail.utoronto.ca>
2020-10-20 19:49:48 -0400

    Added --silent --wait to run_client.py

--- run_client.py
+++ run_client.py
@@ -16,12 +16,12 @@ class ControlPrompt(Cmd):
         self.__procs = procs
 
     def do_list(self, arg=None):
-        print('List of running processes:')
-        print(*sorted(self.__procs.keys()))
+        print('Info:', 'List of running processes:')
+        print('Info:', *sorted(self.__procs.keys()))
         print('')
     
     def do_update(self, arg=None):
-        print('Updating the status of processes..')
+        print('Info:', 'Updating the status of processes..')
         procs = dict()
         for idx, proc in self.__procs.items():
             if proc.poll() is None:
@@ -33,25 +33,25 @@ class ControlPrompt(Cmd):
         return self.check_exit()
 
     def do_exit(self, arg=None):
-        print('Killing all running processes..')
+        print('Warning:', 'Killing running processes.. ALL')
         for idx, proc in self.__procs.items():
             proc.kill()
-            print('    Killed process', idx)
+            print('Warning:', '    Killed process', idx)
         
         self.__procs.clear()
         return self.check_exit()
 
     def do_kill(self, arg):
         request_to_kill = sorted(map(int, arg.split()))
-        print('Try to kill:', *request_to_kill)
+        print('Info:', 'To kill:', *request_to_kill)
 
         to_kill = sorted(set(self.__procs.keys()).intersection(set(request_to_kill)))
-        print('To kill:', *to_kill)
+        print('Warning:', 'Killing running processes..', *to_kill)
 
         for idx in to_kill:
             self.__procs[idx].kill()
             del self.__procs[idx]
-            print('    Killed process', idx)
+            print('Warning:', '    Killed process', idx)
 
         return self.check_exit()
 
@@ -62,42 +62,63 @@ class ControlPrompt(Cmd):
             self.do_list()
 
     def clean_up(self):
-        print('All processes are killed. Exiting')
+        print('Info:')
+        print('Info:', 'All processes are killed. Exiting')
+        print('Info:')
         return True
 
 
-def main():
-    parser = argparse.ArgumentParser(description='run_client.py')
-    parser.add_argument('--cmd', type=str, default='./client', help='Command to run')
-    parser.add_argument('--count', type=int, required=True, help='Number of clients to deploy')
-    parser.add_argument('--out', type=str, help='Place to dump output files for each client. Default is no output')
-    parser.add_argument('--port', type=str, default=':1747', help='Server @<IP>:<PORT>')
-    args = parser.parse_args()
+def main(args):
+    print('Info:', args)
+    print('Info:')
 
-    print(args)
-
-    if args.out:
-        if not os.path.exists(args.out):
-            os.mkdir(args.out)
-        outputs = [open(os.path.join(args.out, 'client_' + str(i) + '.log'), mode='w') for i in range(args.count)]
+    if args.wait:
+        print('Warning:', 'The program will terminate once all subprocesses are terminated')
+    if args.silent: # Suppress all outputs
+        outputs = [subprocess.DEVNULL] * args.count
+        out_place_str = 'supressed'
     else:
-        outputs = [sys.stdout] * args.count
+        if args.output: # Redirect output to files
+            if not os.path.exists(args.output):
+                os.mkdir(args.output)
+            outputs = [open(os.path.join(args.output, 'client_' + str(i) + '.log'), mode='w') for i in range(args.count)]
+            out_place_str = 'redirected into ' + args.output
+        else: # stdout
+            outputs = [sys.stdout] * args.count
+            out_place_str = 'redirected into stdout'
+    print('Info:', 'All output of running processes are', out_place_str)
 
     command = args.cmd.split()
     command.append(args.port)
 
-    print("Launching all processes '" + ' '.join(command) + "'")
+    print('Info:')
+    print('Info:', "Launching all processes '" + ' '.join(command) + "'")
     def launch_job(i):
-        print('    Running process', i)
+        print('Info:', '    Running process', i)
         return subprocess.Popen(command, stdout=outputs[i], stderr=outputs[i])
     procs = [launch_job(i) for i in range(args.count)]
 
-    # turn list of procs to dict of {proc_idx: prc}
-    procs = dict(enumerate(procs))
+    if args.wait:
+        for proc in procs:
+            proc.wait()
+    else:
+        # turn list of procs to dict of {proc_idx: prc}
+        ControlPrompt(dict(enumerate(procs))).cmdloop()
 
-    ControlPrompt(procs).cmdloop()
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='run_client.py')
+    parser.add_argument('--count', type=int, required=True, help='Number of clients to deploy')
+    parser.add_argument('--port', type=str, default=':1747', help='Server @<IP>:<PORT>')
+    parser.add_argument('--cmd', type=str, default='./client', help='Command to run')
+
+    parser.add_argument('--output', type=str, help='Place to dump output files for each client. Default is stdout.')
+    parser.add_argument('--silent', action='store_true', help='Suppress all output. Default is stdout.')
+
+    parser.add_argument('--wait', action='store_true', help='Disable the command shell, exit when all processes are done. Default is command shell.')
+    
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    main()
-
+    main(parse_arguments())
