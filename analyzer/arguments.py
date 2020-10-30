9e2c5599be5525cde3e87493b221560f696da1ee
Lichen Liu <issac.liu@mail.utoronto.ca>
2020-10-30 14:18:08 -0400

    max_row->30000

--- analyzer/arguments.py
+++ analyzer/arguments.py
@@ -3,7 +3,7 @@ import argparse
 
 def load_argument(parser):
     parser.add_argument('--iter_num', type=int, default=100, help='Moving average window size')
-    parser.add_argument('--max_row', type=int, default=14000, help='Number of iterations of raw data to process')
+    parser.add_argument('--max_row', type=int, default=30000, help='Number of iterations of raw data to process')
 
     parser.add_argument('--debug', action='store_true', help='Print debug messages when on')
     parser.add_argument('--gui', action='store_true', help='Open charts on GUI')
